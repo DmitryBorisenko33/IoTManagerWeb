5d68a0b74cd45386b2e82e9601724ea2e006887e
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2021-08-11 03:09:30 +0800

    1

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,7 @@
   <ul class="menu__main">
     <Route path="/">
       <!-- Card Container -->
-      <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <div class="grid place-items-center mx-2 my-5 sm:my-auto">
         <!-- Card -->
         <div class="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
           <!-- Card Title -->
@@ -45,7 +45,7 @@
 
     <Route path="/config">
       <!-- Card Container -->
-      <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <div class="grid place-items-center mx-2 my-5 sm:my-auto">
         <!-- Card -->
         <div class="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
           <!-- Card Title -->
@@ -56,9 +56,9 @@
 
     <Route path="/connection">
       <!-- Card Container -->
-      <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <div class="grid place-items-center mx-2 my-5 sm:my-auto">
         <!-- Card -->
-        <div class="w-11/12 p-12 sm:w-8/12 my-10 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
+        <div class="w-11/12 p-12 sm:w-8/12 my-5 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
           <!-- Card Title -->
           <h1 class="text-center text-lg text-gray-500 font-bold pb-6">Подключение к WiFi роутеру</h1>
           <form class="w-full max-w-sm">
@@ -96,9 +96,9 @@
       </div>
 
       <!-- Card Container -->
-      <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <div class="grid place-items-center mx-2 my-5 sm:my-auto">
         <!-- Card -->
-        <div class="w-11/12 p-12 sm:w-8/12 my-10 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
+        <div class="w-11/12 p-12 sm:w-8/12 my-5 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
           <!-- Card Title -->
           <h1 class="text-center text-lg text-gray-500 font-bold pb-6">Подключение к MQTT серверу</h1>
           <form class="w-full max-w-sm">
